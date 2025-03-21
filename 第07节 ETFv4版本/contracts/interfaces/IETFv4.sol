1162f1b514572f141b2f471abc4de1716db0f9d9
SiLugang <167219827+SiLugang@users.noreply.github.com>
2025-03-21 22:33:31 +0800

    Update IETFv4.sol

--- 第07节 ETFv4版本/contracts/interfaces/IETFv4.sol
+++ 第07节 ETFv4版本/contracts/interfaces/IETFv4.sol
@@ -21,21 +21,21 @@ interface IETFv4 is IETFv3 {
 
     function INDEX_SCALE() external view returns (uint256);
 
-    function miningSpeedPerSecond() external view returns (uint256);
+    function miningSpeedPerSecond() external view returns (uint256);//挖矿的速率
 
-    function miningLastIndex() external view returns (uint256);
+    function miningLastIndex() external view returns (uint256);//挖矿的全局级别的指数----从开始到当前累计的，奖励；每单位的ETF代币可分得的一个相关的指数
 
-    function lastIndexUpdateTime() external view returns (uint256);
+    function lastIndexUpdateTime() external view returns (uint256);//上面的指数的最近的一次更新时间
 
-    function supplierLastIndex(
+    function supplierLastIndex(//用户级别的指数
+        address supplier//每个用户会更新一个指数index
+    ) external view returns (uint256);
+
+    function supplierRewardAccrued(//每个用户累计的奖励，到上市交互为止累计下来的奖励，未领取的奖励？
         address supplier
     ) external view returns (uint256);
 
-    function supplierRewardAccrued(
-        address supplier
-    ) external view returns (uint256);
-
-    function getClaimableReward(
+    function getClaimableReward(//可以查询当前为止，的可领取的奖励---每次查询会有不同的变化
         address supplier
     ) external view returns (uint256);
 }
