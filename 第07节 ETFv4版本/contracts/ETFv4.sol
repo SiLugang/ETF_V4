0cf9babc59384819b00f73cb59e4c7c22b537894
SiLugang <167219827+SiLugang@users.noreply.github.com>
2025-03-21 23:18:50 +0800

    Update ETFv4.sol

--- 第07节 ETFv4版本/contracts/ETFv4.sol
+++ 第07节 ETFv4版本/contracts/ETFv4.sol
@@ -102,49 +102,49 @@ contract ETFv4 is IETFv4, ETFv3 {
         return claimable;
     }
 
-    function _updateMiningIndex() internal {
-        if (miningLastIndex == 0) {
-            miningLastIndex = INDEX_SCALE;
-            lastIndexUpdateTime = block.timestamp;
-        } else {
-            uint256 totalSupply = totalSupply();
-            uint256 deltaTime = block.timestamp - lastIndexUpdateTime;
-            if (totalSupply > 0 && deltaTime > 0 && miningSpeedPerSecond > 0) {
-                uint256 deltaReward = miningSpeedPerSecond * deltaTime;
-                uint256 deltaIndex = deltaReward.mulDiv(
+    function _updateMiningIndex() internal {//updateminingindex的实现
+        if (miningLastIndex == 0) {//如果为0
+            miningLastIndex = INDEX_SCALE;//初始化为INDEX_SCALE
+            lastIndexUpdateTime = block.timestamp;//更新时间
+        } else {//不等于0的时候开始计算，
+            uint256 totalSupply = totalSupply();//总供应
+            uint256 deltaTime = block.timestamp - lastIndexUpdateTime;//时间差
+            if (totalSupply > 0 && deltaTime > 0 && miningSpeedPerSecond > 0) {//判断，三个参数都大于0的情况下：
+                uint256 deltaReward = miningSpeedPerSecond * deltaTime;//每秒挖矿速率*时间差=这段时间给出的奖励数
+                uint256 deltaIndex = deltaReward.mulDiv(//指数等于，奖励差*index精度（1e36）/totalsupply，分给总supply
                     INDEX_SCALE,
                     totalSupply
                 );
-                miningLastIndex += deltaIndex;
-                lastIndexUpdateTime = block.timestamp;
-            } else if (deltaTime > 0) {
-                lastIndexUpdateTime = block.timestamp;
+                miningLastIndex += deltaIndex;//1e36+deltaindex=最新的指数
+                lastIndexUpdateTime = block.timestamp;//更新时间戳
+            } else if (deltaTime > 0) {//其他的两个等于0，delta大于0时
+                lastIndexUpdateTime = block.timestamp;//更新update时间戳
             }
         }
     }
 
-    function _updateSupplierIndex(address supplier) internal {
-        uint256 lastIndex = supplierLastIndex[supplier];
-        uint256 supply = balanceOf(supplier);
-        uint256 deltaIndex;
-        if (lastIndex > 0 && supply > 0) {
-            deltaIndex = miningLastIndex - lastIndex;
-            uint256 deltaReward = supply.mulDiv(deltaIndex, INDEX_SCALE);
-            supplierRewardAccrued[supplier] += deltaReward;
+    function _updateSupplierIndex(address supplier) internal {//用户级别的index逻辑
+        uint256 lastIndex = supplierLastIndex[supplier];//上次的index，supplier（用户？）级别的？
+        uint256 supply = balanceOf(supplier);//用户级别的supply
+        uint256 deltaIndex;//deltaindex，
+        if (lastIndex > 0 && supply > 0) {//指数和supply都大于0时
+            deltaIndex = miningLastIndex - lastIndex;//deltaindex等于上一个miningindex减去上次index
+            uint256 deltaReward = supply.mulDiv(deltaIndex, INDEX_SCALE);//supply*index差，再除以index标尺（1e36）
+            supplierRewardAccrued[supplier] += deltaReward;//累加到用户的奖励中
         }
-        supplierLastIndex[supplier] = miningLastIndex;
+        supplierLastIndex[supplier] = miningLastIndex;//更新用户奖励的index
         emit SupplierIndexUpdated(supplier, deltaIndex, miningLastIndex);
     }
 
     // override ERC20 _update function
-    function _update(
+    function _update(//重写erc20的Update函数---mint，burn，transfer的时候都会用到
         address from,
         address to,
         uint256 value
     ) internal override {
-        _updateMiningIndex();
-        if (from != address(0)) _updateSupplierIndex(from);
-        if (to != address(0)) _updateSupplierIndex(to);
+        _updateMiningIndex();//更新全局的什么指数？
+        if (from != address(0)) _updateSupplierIndex(from);//from地址不为0的话，更新给用户级别的index
+        if (to != address(0)) _updateSupplierIndex(to);//to也会更新，to的index？------
         super._update(from, to, value);
     }
 }
